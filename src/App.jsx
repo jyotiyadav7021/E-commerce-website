0424e7957738b0701bc214ca5187d59f45ac9837
JyotiYadav <57616116+jyotiyadav7021@users.noreply.github.com>
2025-04-28 21:19:13 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@
 import React from 'react'
 import { BrowserRouter,Routes, Route } from 'react-router'
 import HomePage from './pages/HomePage'
-
+import ErrorPage from './pages/ErrorPage'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 function IndexPage() {
   
 
@@ -12,7 +14,11 @@ function IndexPage() {
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<HomePage/>}></Route>
+        <Route path='/' component={<ErrorPage/>}></Route>
       </Routes>
+      
+      <Navbar></Navbar>
+      <Footer></Footer>
       </BrowserRouter>
     </>
   )
